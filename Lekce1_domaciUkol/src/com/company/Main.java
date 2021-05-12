40e9a1622cc56f3f1ab4836f7ac368c838792a01
MichalBorisov <mikeborissov@gmail.com>
2021-05-12 23:54:03 +0200

    added projects from first Lesson

---
+++ Lekce1_domaciUkol/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.math.BigDecimal;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+public class Main {
+    public static void main(String[] args)
+    {
+        String clientName;
+        int amountOfOrders;
+        String companyCity;
+        BigDecimal carFuelConsumption;
+        int cableLength;
+        LocalDate clientDateOfBirth;
+        String carPlate;
+        int clientIdentityNumber;
+    }
+}
